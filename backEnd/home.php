b9de6c1bd5d735b83dbb3d9db86301027ac45ba7
Ashen Shanuka <shanuka-im19008@stu.kln.ac.lk>
2022-11-11 23:49:16 +0530

    lkr space added

--- backEnd/home.php
+++ backEnd/home.php
@@ -141,7 +141,7 @@ include 'components/add_cart.php';
          <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
          <div class="name"><?= $fetch_products['name']; ?></div>
          <div class="flex">
-            <div class="price"><span>LKR</span><?= $fetch_products['price']; ?></div>
+            <div class="price"><span>LKR </span><?= $fetch_products['price']; ?></div>
             <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
          </div>
       </form>
