e74c49c98785e2cfd052d03ea31cfe29cc5577c9
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-11-11 19:24:40 +0530

    db cred updated

--- backEnd/components/connect.php
+++ backEnd/components/connect.php
@@ -2,7 +2,7 @@
 
 $db_name = 'mysql:host=localhost;dbname=food_db';
 $user_name = 'root';
-$user_password = '';
+$user_password = 'Nappa20000614';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
